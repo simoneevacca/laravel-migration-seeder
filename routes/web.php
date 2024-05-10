2640decf25478afa7b11f2b4f650690432ef21c4
simoneevacca <v.simone.vacca@gmail.com>
2024-05-10 15:33:16 +0200

    add controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guests\TrainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/train', [TrainController::class, 'index']);
